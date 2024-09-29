859a17e8f5b30b2a288a70c9162700413965ca15
felixng <ngthinh302@gmail.com>
2024-09-29 16:54:21 -0400

    video inference + videos

--- src/video_inference.py
+++ src/video_inference.py
@@ -23,6 +23,24 @@ parser.add_argument(
     choices=["distill"],
     help="Model architecture",
 )
+parser.add_argument(
+    "--input",
+    type=str,
+    default="fake.mp4",
+    help="Path to the input video file",
+)
+parser.add_argument(
+    "--output",
+    type=str,
+    default="output.mp4",
+    help="Path to the output video file",
+)
+parser.add_argument(
+    "--fps",
+    type=float,
+    default=30,
+    help="Frames per second for the output video",
+)
 args = parser.parse_args()
 
 
@@ -31,8 +49,8 @@ def video_inference():
     models = AdversarialModels(args)
     models.load_ckpt()
 
-    cap = cv2.VideoCapture("fake.mp4")
-    res = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 15, (512, 256))
+    cap = cv2.VideoCapture(args.input)
+    res = cv2.VideoWriter(args.output, cv2.VideoWriter_fourcc(*"mp4v"), args.fps, (512, 256))
 
     # i = 0
 
@@ -46,26 +64,23 @@ def video_inference():
         if not ret:
             print("finished. saving...")
             break
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        im_pil = Image.fromarray(rgb_frame)
+        # rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # im_pil = Image.fromarray(rgb_frame)
+        im_pil = Image.fromarray(frame)
         rgb = transforms.ToTensor()(im_pil).to(device)
         rgb = F.resize(
             rgb,
             [512, 256],
-        ).unsqueeze(0)
-        disp = models.distill(rgb)
-        visualize_disparity(rgb, disp)
-
-        disp = (
-            F.resize(
-                disp,
-                [disp.shape[-1], disp.shape[-2]],
-            )
-            .squeeze()
-            .detach()
-            .cpu()
-            .numpy()
         )
+        disp = models.distill(rgb)
+        # remove the batch dimension
+        # disp = disp.squeeze(0)
+        # rgb = rgb.squeeze(0)
+        # print("disp", disp.shape)
+        # print("rgb", rgb.shape)
+        # visualize_disparity(rgb, disp)
+
+        disp = disp.squeeze().detach().cpu().numpy()
         disp_normalized = cv2.normalize(disp, None, 0, 255, cv2.NORM_MINMAX).astype(
             np.uint8
         )
@@ -73,7 +88,7 @@ def video_inference():
         disp_color = cv2.applyColorMap(disp_normalized, cv2.COLORMAP_PLASMA)
         # cv2.imshow("Disparity", disp_color)
 
-        print("here", disp_color.shape) # (256, 512, 3)
+        print("here", disp_color.shape)  # (256, 512, 3)
         disp_color = cv2.resize(disp_color, (512, 256))
         res.write(disp_color)
         if cv2.waitKey(1) == ord("q"):
